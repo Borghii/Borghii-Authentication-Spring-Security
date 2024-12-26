59c7a2de398829fde86c0d31a2e837b5257bd8db
Borghi <137845283+Borghii@users.noreply.github.com>
2024-12-25 21:51:35 -0300

    uploadign proyect

---
+++ src/main/java/com/authentication/borghi/BorghiApplication.java
@@ -0,0 +1,13 @@
+package com.authentication.borghi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BorghiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BorghiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/authentication/borghi/BorghiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.authentication.borghi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BorghiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
