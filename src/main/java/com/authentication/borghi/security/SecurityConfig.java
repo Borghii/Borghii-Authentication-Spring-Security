5ed00a99c68ed054cc7dd72de97cbf6f7253c242
Borghi <137845283+Borghii@users.noreply.github.com>
2024-12-26 18:13:40 -0300

    feat: authenticate user db default with bcrypt

--- src/main/java/com/authentication/borghi/security/SecurityConfig.java
+++ src/main/java/com/authentication/borghi/security/SecurityConfig.java
@@ -4,10 +4,23 @@ import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
+import org.springframework.security.provisioning.UserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+
+import javax.sql.DataSource;
 
 @Configuration
 public class SecurityConfig {
+
+
+
+    //add suppor for jdbd no more hardorded users
+    @Bean
+    public UserDetailsManager userDetailsManager(DataSource dataSource){
+        return new JdbcUserDetailsManager(dataSource);
+    }
 
 
     @Bean
