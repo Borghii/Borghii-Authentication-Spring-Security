d7ef7b652357474897341a4b13469a307eab5f6e
Borghi <137845283+Borghii@users.noreply.github.com>
2024-12-26 00:17:37 -0300

    feat: custom login

--- src/main/java/com/authentication/borghi/security/SecurityConfig.java
+++ src/main/java/com/authentication/borghi/security/SecurityConfig.java
@@ -1,5 +1,7 @@
 package com.authentication.borghi.security;
 
+import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
@@ -7,14 +9,27 @@ import org.springframework.security.web.SecurityFilterChain;
 @Configuration
 public class SecurityConfig {
 
+
+    @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 
-        http.authorizeHttpRequests((authorize) -> authorize
-                .anyRequest().authenticated()
-        );
+    http.authorizeHttpRequests((authorize) -> authorize
 
-        return http.build();
-    }
+                    //gracias a esto me carga el css (no es necesario autenticacion para recurso estaticos)
+                    .requestMatchers(PathRequest.toStaticResources().atCommonLocations()).permitAll()
+                    .anyRequest().authenticated()
+            )
+            .formLogin(form->
+                    form
+                            .loginPage("/showMyCustomLogin")
+                            .loginProcessingUrl("/authenticateTheUser")
+                            .permitAll()
+            );
+
+    return http.build();
+}
+
+
 
 
 }
